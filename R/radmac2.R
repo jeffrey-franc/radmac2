95ce19332f3bbea89b29f1651ec89abc24334063
Jeffrey Franc <jef@medstatstudio.com>
2016-05-14 17:32:25 +0000

    Adds Patient volumes function

--- R/radmac2.R
+++ R/radmac2.R
@@ -4,27 +4,59 @@ los<-function(x,...){
 
 
 
-los<-function(protocol,sim,db='medstat1_surgesim'){
+
+
+
+los<-function(db='medstat1_surgesim',sim,protocol){
+    #' Calculate length of stay benchmarks for SurgeSim
+    #'
+    #' @param db Main Simulation database containing datamine
+    #' @param sim Simulation database
+    #' @param protocol Simulation protocol for comparison
+    #' @examples
+    #' z<-los(protocol='Geyserville',sim='unmert');
+    surgesimcalc(db=db,sim=sim,protocol=protocol,rtype='los');
+}
+
+pv<-function(db='medstat1_surgesim',sim,protocol){
+    #' Calculate patient volume stay benchmarks for SurgeSim
+    #'
+    #' @param db Main Simulation database containing datamine
+    #' @param sim Simulation database
+    #' @param protocol Simulation protocol for comparison
+    #' @examples
+    #' z<-pv(protocol='Geyserville',sim='unmert');
+    surgesimcalc(db=db,sim=sim,protocol=protocol,rtype='pv');
+}
+
+
+surgesimcalc<-function(db='medstat1_surgesim',sim,protocol,rtype){
 
 # This Sim
     sqlstriage<-"SELECT RegTime-Time_Runtime AS triage FROM runtimes where RegTime-Time_Runtime < 3600 and RegTime IS NOT NULL";
     striage_df <- mydf(sql=sqlstriage,db=sim);
     triage<-median(striage_df$triage);
+    ntriage<-length(striage_df$triage);
+
 
      sqlsroom<-"SELECT RoomTime-Time_Runtime AS room FROM runtimes where RoomTime-Time_Runtime < 3600 and RoomTime IS NOT NULL";
     sroom_df <- mydf(sql=sqlsroom,db=sim);
     room<-median(sroom_df$room);
+    nroom<-length(sroom_df$room);
 
 sqlsmd<-"SELECT MDTime-Time_Runtime AS md FROM runtimes where MDTime-Time_Runtime < 3600 and MDTime IS NOT NULL";
     smd_df <- mydf(sql=sqlsmd,db=sim);
     md<-median(smd_df$md);
+    nmd<-length(smd_df$md);
 
     sqlsdispo<-"SELECT DispoTime-Time_Runtime AS dispo FROM runtimes where DispoTime-Time_Runtime < 3600 and DispoTime IS NOT NULL";
     sdispo_df <- mydf(sql=sqlsdispo,db=sim);
     dispo<-median(sdispo_df$dispo);
+    ndispo<-length(sdispo_df$dispo);
 
-    values=c(triage=triage,room=room,md=md,dispo=dispo);
 
+    values<-c(triage=triage,room=room,md=md,dispo=dispo);
+    nvalues<-c(triage=ntriage,room=nroom,md=nmd,dispo=ndispo);
 
 
 
@@ -37,6 +69,9 @@ sqlsmd<-"SELECT MDTime-Time_Runtime AS md FROM runtimes where MDTime-Time_Runtim
     triage_df<-mydf(sql=sqltriage,db=db);
     triage_med <-median(triage_df$triage);
     triage_q75<-as.numeric(quantile(triage_df$triage)[4]);
+    ntriage<-(with(triage_df,aggregate(triage,by=list(Simulation),FUN=length))[2])[,1];
+    ntriage_med<-median(ntriage);
+    ntriage_q25 <- as.numeric(quantile(ntriage))[2];
 
     #Room
      sqlroom<-"select Simulation,Protocol,RoomTime-Time_Runtime AS room FROM datamine RIGHT join stat_TOC ON datamine.Simulation=stat_toc.DBName where RoomTime IS NOT NULL && RoomTime-Time_Runtime < 3600 && Protocol = '";
@@ -45,7 +80,9 @@ sqlsmd<-"SELECT MDTime-Time_Runtime AS md FROM runtimes where MDTime-Time_Runtim
     room_df <- mydf(sql=sqlroom, db=db);
     room_med <- median(room_df$room);
     room_q75<-as.numeric(quantile(room_df$room)[4]);
-
+    nroom<-(with(room_df,aggregate(room,by=list(Simulation),FUN=length))[2])[,1];
+    nroom_med<-median(nroom);
+    nroom_q25 <- as.numeric(quantile(nroom))[2];
 
     #MD
 sqlmd<-"select Simulation,Protocol,MDTime-Time_Runtime AS md FROM datamine RIGHT join stat_TOC ON datamine.Simulation=stat_toc.DBName where MDTime IS NOT NULL && MDTime-Time_Runtime < 3600 && Protocol = '";
@@ -54,6 +91,9 @@ sqlmd<-"select Simulation,Protocol,MDTime-Time_Runtime AS md FROM datamine RIGHT
     md_df <- mydf(sql=sqlmd, db=db);
     md_med <- median(md_df$md);
     md_q75<-as.numeric(quantile(md_df$md)[4]);
+    nmd<-(with(md_df,aggregate(md,by=list(Simulation),FUN=length))[2])[,1];
+    nmd_med<-median(nmd);
+    nmd_q25 <- as.numeric(quantile(nmd))[2];
 
 
     #dispo
@@ -63,21 +103,41 @@ sqldispo<-"select Simulation,Protocol,DispoTime-Time_Runtime AS dispo FROM datam
     dispo_df <- mydf(sql=sqldispo, db=db);
     dispo_med <- median(dispo_df$dispo);
     dispo_q75<-as.numeric(quantile(dispo_df$dispo)[4]);
+    ndispo<-(with(dispo_df,aggregate(dispo,by=list(Simulation),FUN=length))[2])[,1];
+    ndispo_med<-median(ndispo);
+    ndispo_q25 <- as.numeric(quantile(ndispo))[2];
+
 
 
     q75<-c(triage=triage_q75,room=room_q75,md=md_q75,dispo=dispo_q75);
     medians<-c(triage=triage_med,room=room_med,md=md_med,dispo=dispo_med);
+    nmedians<-c(triage=ntriage_med,room=nroom_med,md=nmd_med,dispo=ndispo_med);
+    nq25<-c(triage=ntriage_q25,room=nroom_q25,md=nmd_q25,dispo=ndispo_q25);
 
-
-
-
+#returns
     z<-(list(simulation=sim,
              medians=medians,
              q75=q75,
              values=values
              ));
-    class(z)<-'los';
-    return(z);
+
+    z2<-(list(simulation=sim,
+              medians=nmedians,
+              q25=nq25,
+              values=nvalues
+             ));
+
+
+
+
+   class(z)<-'los';
+   class(z2)<-'pv';
+
+    if(rtype=='los'){
+        return(z);
+    }else{
+        return(z2);
+    }
 
 
 }
@@ -101,6 +161,28 @@ plot.los <- function(z,color=TRUE){
     legend(x=q[1],y=ymax,legend=c('Median','75%ile'),lty=c(1,3),col=c(plot_colors[2],plot_colors[3]));
 
 }
+
+plot.pv <- function(z,color=TRUE){
+    maintitle<-paste('SurgeSim Patient Volume Markers for',z$simulation);
+
+    if(color){
+         plot_colors<-c('blue','green','red');
+
+    }else{
+       plot_colors<-c('grey','black','black');
+    }
+
+    ymax<-(max(c(z$medians,z$values,z$q25)))+2;
+
+
+    q<-barplot(z$values,main=maintitle,ylab='Number of Patients to Reach Benchmark',ylim=c(0,ymax),col=plot_colors[1],sub='Values below the 25%ile may warrent investigation');
+    lines(x=q,y=z$medians,type='o',lty=1,col=plot_colors[2]);
+    lines(x=q,y=z$q25,type='o',lty=3,col=plot_colors[3]);
+    legend(x=q[3],y=ymax,legend=c('Median','25%ile'),lty=c(1,3),col=c(plot_colors[2],plot_colors[3]));
+
+}
+
+
 
 
 
